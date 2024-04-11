62245623dab1a42f8caa8c1c58ef397f9a69082f
o-spencer <166622685+o-spencer@users.noreply.github.com>
2024-04-11 09:41:57 -0400

    Create Simon.js
    April 11

---
+++ Simon.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(600, 600);
+  const sequence = []
+  console.log(Math.random(4))
+
+}
+
+function draw() {
+  background("skyBlue");
+}
+
+dog = "dog";
+if (dog == "dog") {
+  
+}
